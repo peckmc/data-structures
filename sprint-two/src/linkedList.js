5f6e9a0a23b50d39115c98b11d5a645b275d849c
mcpeck <peckmelodie@gmail.com>
2022-09-21 20:04:32 -0500

    Complete Linked List & Tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    if (list.tail) {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    } else if (list.tail === null) {
+      list.tail = newNode;
+      list.head = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var oldHead = list.head;
+    list.head = list.head.next;
+    return oldHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -26,4 +47,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addToTail = O(1)
+ * removeHead = O(1)
+ * contains = O(n)
  */
