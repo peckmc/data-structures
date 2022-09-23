69c00a2db8d98935fb273d88207470ea085fbeaa
mcpeck <peckmelodie@gmail.com>
2022-09-22 19:23:08 -0500

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,36 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if(_.every(this._storage, function(element) {
+    return element !== item;
+  })) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  var itemIndex = this._storage.indexOf(item);
+  this._storage.splice(itemIndex, 1);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add: O(1)
+ * contains: O(n)
+ * remove: O(n)
  */
